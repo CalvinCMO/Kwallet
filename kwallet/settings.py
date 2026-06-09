1b066ded9bbe324be83823fb1536a6d86e636aa2
CalvinCMO <calvinmanyasa54@gmail.com>
2026-06-09 14:10:12 +0300

    Update settings

--- kwallet/settings.py
+++ kwallet/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY',
     'django-insecure-dev-only-kwallet-v2-change-in-production')
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
